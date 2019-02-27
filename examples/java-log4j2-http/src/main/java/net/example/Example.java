f1fd00ccf269dd7b7e6b8bac2dd299b2ee567544
Rui Lopes <rgl@ruilopes.com>
2019-02-27 21:46:01 +0000

    add the java-log4j2-http example

---
+++ examples/java-log4j2-http/src/main/java/net/example/Example.java
@@ -0,0 +1,49 @@
+// see https://logging.apache.org/log4j/2.x/manual/usage.html
+// see https://logging.apache.org/log4j/2.x/manual/thread-context.html
+
+package net.example;
+
+import java.util.UUID;
+import org.apache.logging.log4j.CloseableThreadContext;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
+
+public class Example {
+    final static Logger log = LogManager.getLogger(Example.class);
+
+    public static void main(String[] args) throws Exception {
+        String application = "java-log4j2-http/1.0";
+
+        ThreadContext.put("application", application);
+
+        log.info("Begin");
+
+        String traceId = UUID.randomUUID().toString().replace("-", "");
+
+        try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.put("traceId", traceId)) {
+            int a = 10;
+            int b = 0;
+
+            try {
+                log.debug("Dividing {} by {}", a, b);
+                System.out.println(a / b);
+            } catch (Exception ex) {
+                log.error("Something went wrong with the division", ex);
+            }
+        }
+
+        log.info(getLongMessage("long message ", 2*1024));
+
+        log.info("End");
+    }
+
+    private static String getLongMessage(String prefix, int width) {
+        StringBuffer buffer = new StringBuffer(width);
+        buffer.append(prefix);
+        for (int i = 0; i < width-prefix.length(); i++) {
+            buffer.append(i%10);
+        }
+        return buffer.toString();
+    }
+}
