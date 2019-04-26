84b57fb30b21c33a27c160ec7917156e83ccec6b
Rui Lopes <rgl@ruilopes.com>
2019-04-26 12:45:24 +0100

    upgrade to npcap 0.992

--- provision-wireshark.ps1
+++ provision-wireshark.ps1
@@ -2,8 +2,8 @@ choco install -y wireshark
 
 # leave npcap on the desktop for the user to install manually.
 # (it does not have a silent installer).
-$url = 'https://nmap.org/npcap/dist/npcap-0.99-r9.exe'
-$expectedHash = 'fa35408528c4254cec6104f25f59b01fb7d35cd153507fc54a7291edc8824f7f'
+$url = 'https://nmap.org/npcap/dist/npcap-0.992.exe'
+$expectedHash = 'a41d6e8296ef55c71fc04c5775e5d0e5d04689fc2aaf7f6f908664db9670a1f9'
 $localPath = "$env:USERPROFILE\Desktop\$(Split-Path -Leaf $url)"
 (New-Object Net.WebClient).DownloadFile($url, $localPath)
 $actualHash = (Get-FileHash $localPath -Algorithm SHA256).Hash
